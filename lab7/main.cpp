82ff07677400ed45998e85ba42b23384c7d53f00
boon4681 <boon4681dev@gmail.com>
2026-08-29 14:18:25 +0700

    forgor

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -29,3 +29,24 @@ bullet(lab7_quest12)
     }
     return 0;
 }
+
+bullet(lab7_quest13)
+{
+    ImBMP img1("./resource/AliasingPattern.bmp");
+    if (!img1.ok())
+    {
+        return 1;
+    }
+    img1.resizeNearestNeighbour(0.25, 0.25);
+    img1.write("./out/aliasing-" + bullet_name + "-noblur.bmp");
+
+    ImBMP img2("./resource/AliasingPattern.bmp");
+    if (!img2.ok())
+    {
+        return 1;
+    }
+    img2.averagingFilter(3);
+    img2.resizeNearestNeighbour(0.25, 0.25);
+    img2.write("./out/aliasing-" + bullet_name + "-blurred.bmp");
+    return 0;
+}
