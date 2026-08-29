4fb1dd835ac445d63ad7e8736bd9cd6c83363b63
boon4681 <boon4681dev@gmail.com>
2026-08-29 11:07:05 +0700

    update

--- gun.cpp
+++ gun.cpp
@@ -1,7 +1,16 @@
 // include script to load as a bullet
-#include "./lab2/main.cpp"
+// #include "./lab2/main.cpp"
 #include "./lab3/main.cpp"
+// #include "./lab4/main.cpp"
 // #include "./lab5/main.cpp"
+// #include "./lab6/main.cpp"
+// #include "./lab7/main.cpp"
+// #include "./lab8/main.cpp"
+// #include "./lab9/main.cpp"
+// #include "./lab10/main.cpp"
+// #include "./lab11/main.cpp"
+// #include "./lab12/main.cpp"
+// #include "./lab13/main.cpp"
 
 #include <cstdio>
 #include "./bullet.hpp"
